80bc534a4d762c8132c355690f605191a0d029d7
clon54programer <112908720+clon54programer@users.noreply.github.com>
2022-11-22 16:49:30 -0500

    Add project files.

---
+++ ForCicle/Program.cs
@@ -0,0 +1,28 @@
+﻿
+// los ciclos for se componen por una varible, una condicion y la candtiafda de  repeticiones
+
+for (int i = 0/*condicion inicial*/; i < 50/*limite de repeticiones*/; i= i+10/*incremento*/) 
+{
+    Console.WriteLine("hello world #{0}", i);
+}
+
+
+
+for (int a= 0; a <= 50; a++)
+{
+    int residuo = (a % 2);
+    if(residuo == 0)
+    {
+        string mensaje = " es par";
+        Console.WriteLine("el numero "+a+ mensaje);
+    }
+    else
+    {
+        string mensaje = " es impar";
+        Console.WriteLine("el numero " + a + mensaje);
+    }   
+}
+for (int i = 0, j = 0; i < 10 && j > -10; i++, j--)
+{
+    Console.WriteLine("i: " + i + " j: " + j);
+}
